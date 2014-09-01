48955c3fdd3eb4b468b9d61cdb6d5ea3851b0613
Jonas Osborn <jonas.w.osborn@gmail.com>
2014-09-01 18:53:54 +0100

    Have vim module install molokai colourscheme

--- vim/install.sh
+++ vim/install.sh
@@ -1,12 +1,12 @@
 setup_vundle () {
-	if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
-	then
-		info 'Setting up Vundle'
+	info 'Setting up Vundle'
 
-		mkdir -p $HOME/.vim/bundle
-		git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-		success 'Vundle installed'
-	fi
+	mkdir -p $HOME/.vim/bundle
+	git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+	success 'Vundle installed'
 }
 
-setup_vundle
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then setup_vundle; fi
+
+if [ ! -d "$HOME/.vim/colors" ]; then mkdir -p "$HOME/.vim/colors"; fi
+link_file "$DOTFILES_ROOT/vim/molokai.vim" "$HOME/.vim/colors/molokai.vim"
